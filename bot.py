4c21c7f82cedc5ccea4d6f1d8faab18ad22d7c4f
required <gleb.krasnow@ya.ru>
2024-05-25 15:17:59 +0300

    создал счетчик и выввод 2 вопросов в переменную

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ bot = telebot.TeleBot(token)
 
 answer = [] #переменная для ответов из бд
 
-
 try:
     connection = psycopg2.connect(
         host=host,
@@ -43,13 +42,26 @@ def start(message):
 
 @bot.callback_query_handler(func=lambda call: call.data == 'faq_all')
 def faq_all_btn(call):
+    question_id = 0
     markup = types.InlineKeyboardMarkup()
     message = call.message
     chat_id = message.chat.id
     for question in questions:
-        btn = types.InlineKeyboardButton(text=question, callback_data='answer_')# + str(question_id))  # Присваиваем каждой кнопке уникальный callback_data
+        question_id += 1
+        btn = types.InlineKeyboardButton(text=question, callback_data='answer_' + str(question_id))# Присваиваем каждой кнопке уникальный callback_data
         markup.add(btn)
-    bot.send_message(chat_id, f'dab dab', reply_markup=markup)
-
-
+    bot.send_message(chat_id, f'Какой тип вопросов вам подходит?', reply_markup=markup)
+@bot.callback_query_handler(func=lambda call: 'answer_' in call.data)
+def faq_theme_btn(call):
+    question_number = call.data[-1]
+    connection = psycopg2.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=db_name
+    )
+    with connection.cursor() as cursor:
+        cursor.execute(f"SELECT questions FROM faq_ans WHERE type_id1 = '{question_number}';")
+        questions2 = cursor.fetchall()
+        print(questions2)
 bot.polling(none_stop = True)
